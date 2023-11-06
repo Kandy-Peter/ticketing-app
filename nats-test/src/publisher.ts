55bc774a2d4fbb1ff56853c21066b0d5db62a141
kandy <kandypeter03@gmail.com>
2023-11-06 19:52:01 +0200

    Create publisher and listener for nats streaming

---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,23 @@
+import nats from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
+console.clear();
+const id = randomBytes(4).toString('hex');
+
+const stan = nats.connect('ticketing', id, {
+  url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+  console.log('Publisher connected to NATS');
+
+  const data = JSON.stringify({
+    id: '123',
+    title: 'concert',
+    price: 20,
+  });
+
+  stan.publish('ticket:created', data, () => {
+    console.log('Event published');
+  });
+})
